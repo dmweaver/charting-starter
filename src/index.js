6e4e3be0d55b694d51e9c9d134edad4acafef3c1
weaverd <david.weaver@dpird.wa.gov.au>
2022-10-24 20:08:46 +0800

    Colour formatted with data from FM

--- src/index.js
+++ src/index.js
@@ -4,38 +4,57 @@ let chart;
 window.loadChart = function (json) {
   const obj = JSON.parse(json);
   const { series, type, callback } = obj;
+  console.log(obj);
+  console.log(obj.series);
 
   //GLOBAL OPTIONS!!!!
 
   const options = {
-    colors: ["#F7DF1E", "#1D91B2"],
-    // stroke: {
-    //   curve: "smooth",
-    //   width: 2,
-    //   dashArray: 5,
-    // },
-    grid: { show: false },
-    // plotOptions: {
-    bar: { horizontal: true, dataLabels: { position: "bottom" } },
-    // },
-    // markers: { size: 3 },
+
+    plotOptions: {
+      bar: {
+          horizontal: false,
+          columnWidth: '95%',
+          borderRadius: 9,
+          dataLabels: { position: "center" }
+      },    
+  },
+  grid:{show:true, borderColor: '#90A4AE'},
+    stroke: {
+      show: true,
+      colors: ["#000"],
+      curve: "smooth",
+      width: 1,
+      dashArray: 0,
+    },
+  
     dataLabels: {
       enabled: true,
+      style:{fontSize:'12px'},
+      
       background: { enabled: true, foreColor: "black" },
       // offsetX: 20,
     },
-    // legend: {
-    //   show: true,
-    //   position: "left",
-    //   horizontalAlign: "right",
-    // },
+    legend: {
+      show: false,
+      position: "left",
+      horizontalAlign: "right",
+    },
     chart: {
       toolbar: { show: false },
       type: type,
-      width: "100%",
-      zoom: { enabled: false },
+      width: 900,
       height: 700,
-      stacked: true,
+      zoom: { enabled: false },
+      stacked: false,
+     
+      dropShadow: {
+        enabled: true,
+        top: 0,
+        left: 0,
+        blur: 4,
+        opacity: 0.7
+      },
       events: {
         click: function (event, chartContext, config) {
           console.log("config");
@@ -43,17 +62,38 @@ window.loadChart = function (json) {
         },
       },
     },
+   
     xaxis: {
+      axisBorder: {
+        show: true,
+        height: 1,
+        color: '#000000',
+        offsetX: 0,
+        offsetY: 0
+    },
       type: "category",
     },
-  };
+    yaxis: {
+    labels:{style:{fontSize: '14px'}},
+    title:{text:'Area (ha)', style:{fontSize: '18px'}},
+    showAlways: true,
+    opposite: false,
+      axisBorder: {
+        show: true,
+        color: '#000000',
+        offsetX: 0,
+        offsetY: 0,
+    }}};
   //OPTIONS FOR XY Charts
   const dataXYOptions = {
     series: series,
     xaxis: {
       type: "category",
+      title: {text: 'P status', style:{fontSize: '18px'}},
+   
     },
   };
+
 
   chart = new ApexCharts(document.querySelector("#chart"), {
     ...options,
