afe927921c1698febe4d41d052b9c0c3dd9d79e8
weaverd <david.weaver@dpird.wa.gov.au>
2022-11-11 21:30:13 +0800

    No animation

--- src/index.js
+++ src/index.js
@@ -27,98 +27,108 @@ window.loadChart = function (json) {
       width: 1,
       dashArray: 0,
     },
-  
+    
     dataLabels: {
       enabled: true,
-      style:{fontSize:'16px'},
+      style:{fontSize:'24px'},
       
       background: { enabled: true, foreColor: "black" },
       // offsetX: 20,
     },
-    legend: {
-      show: false,
-      position: "left",
-      horizontalAlign: "right",
-    },
     chart: {
-      toolbar: { show: false },
-      type: type,
-      width: 800,
-      height: 800,
-      zoom: { enabled: false },
-      stacked: false,
-     
-      dropShadow: {
-        enabled: true,
-        top: 0,
-        left: 0,
-        blur: 4,
-        opacity: 0.7
-      },
-
-      noData: {
-        text: undefined,
-        align: 'center',
-        verticalAlign: 'middle',
-        offsetX: 0,
-        offsetY: 0,
-        style: {
-          color: undefined,
+      legend: {
+        showForSingleSeries: true,
+        //show: true,
+          position: "top",
           fontSize: '14px',
-          fontFamily: undefined
-        }
-      },
-
-      events: {
-        click: function (event, chartContext, config) {
-          console.log("config");
-          handleClick(config, series, callback);
+          //customLegendItems: ['L', 'M', 'H', 'VH', 'E'],
+            // markers: {
+            //     fillColors: ['#00E396', '#775DD0']}
+          
+          },
+          animations:{enabled: false},
+          toolbar: { show: false },
+          type: type,
+          width: 800,
+          height: 800,
+          zoom: { enabled: false },
+          stacked: false,
+          
+          dropShadow: {
+            enabled: true,
+            top: 0,
+            left: 0,
+            blur: 4,
+            opacity: 0.7
+          },
+          
+          noData: {
+            text: undefined,
+            align: 'center',
+            verticalAlign: 'middle',
+            offsetX: 0,
+            offsetY: 0,
+            style: {
+              color: undefined,
+              fontSize: '14px',
+              fontFamily: undefined
+            }
+          },
+          
+          events: {
+            click: function (event, chartContext, config) {
+              console.log("config");
+              handleClick(config, series, callback);
+            },
+          },
         },
-      },
-    },
-   
-    xaxis: {
-      axisBorder: {
-        show: true,
-        height: 1,
-        color: '#000000',
-        offsetX: 0,
-        offsetY: 0
-    },
-  
-   
-      type: "category",
-    },
-    yaxis: {
-    labels:{style:{fontSize: '18px'}},
-    title:{text:'Area (ha)', style:{fontSize: '20px'}},
-    showAlways: true,
-    opposite: false,
-      axisBorder: {
-        show: true,
-        color: '#000000',
-        offsetX: 0,
-        offsetY: 0,
-    }}};
-  //OPTIONS FOR XY Charts
-  const dataXYOptions = {
-    series: series,
-    xaxis: {
-      type: "category",
-      labels:{rotate: -90,
-        rotateAlways: false, 
-        trim: true, 
-        hideOverlappingLabels: false, 
-        style:{fontSize: '18px'},
-        minHeight: 120,
-        offsetY: 0
-      },
-      // title: {text: 'Phosphorus Status', 
-      //   style:{fontSize: '20px'}, 
-      //   align: 'left', 
-      //   offsetY: -700},
-      //   floating: true
-      // eslint-disable-next-line no-dupe-keys
+        
+        xaxis: {
+          axisBorder: {
+            show: true,
+            height: 1,
+            color: '#000000',
+            offsetX: 0,
+            offsetY: 0
+          },
+          
+          
+          type: "category",
+        },
+        yaxis: {
+          labels:{style:{fontSize: '24px'}},
+          title:{text:'Area (ha)', style:{fontSize: '24px'}},
+          showAlways: true,
+          opposite: false,
+          axisBorder: {
+            show: true,
+            color: '#000000',
+            offsetX: 0,
+            offsetY: 0,
+          }}};
+          
+          //OPTIONS FOR XY Charts
+          const dataXYOptions = {
+            series: series,
+            xaxis: {
+              type: "category",
+              //categories: ['L', 'M', 'H', 'VH', 'E'],
+              labels:{
+                rotate: -90,
+                show: true,
+                rotateAlways: false, 
+                trim: false, 
+                hideOverlappingLabels: false, 
+                style:{fontSize: '24px'},
+                minHeight: 120,
+                offsetY: 0
+              },
+            //   title: {text: 'Phosphorus Status', 
+            //   style:{fontSize: '24px'},
+            // align: 'left', 
+            // offsetY: -770},
+            // floating: true,
+              // eslint-disable-next-line no-dupe-keys
       
     },
   };
